9aa93bf889707b62e9f30293805f9e74ab127e8b
tarun-pacifica <tarun@pacificasearch.com>
2014-07-03 23:43:20 +1000

    create new user automatically creates new session so no need to login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,13 @@ class UsersController < ApplicationController
 
    def index
 	  @users = User.all
-   end 
+   end
 
    def create
    	@user = User.new(user_params)
     if @user.save
-      redirect_to user_path
+      session[:user_id] = @user.id
+      redirect_to root_path
     else
       render :new
     end
